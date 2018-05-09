038079fbf692dd7081cf757e30163ac1761c080a
hcrashmi <37067788+hcrashmi@users.noreply.github.com>
2018-05-09 17:30:45 +0530

    Log4j:::: Normal report(text report) and HTML report

---
+++ src/test/java/demo/Log4jHtmlLayoutTest.java
@@ -0,0 +1,17 @@
+package demo;
+
+import org.apache.log4j.Logger;
+import org.apache.log4j.PropertyConfigurator;
+
+public class Log4jHtmlLayoutTest 
+{
+	static Logger log = Logger.getLogger(Log4jHtmlLayoutTest.class);
+	 
+	 public static void main(String[] args) {
+	  
+	  PropertyConfigurator.configure("./log4j.properties");
+	  log.debug("Sample debug message");
+	        log.info("Sample info message");
+	        log.error("Sample error message");
+	     }
+}
